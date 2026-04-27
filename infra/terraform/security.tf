1f49a4e64d1e8ff392b3c3befb1da76230c50b51
jertimothy233 <jertimothy233@gmail.com>
2026-04-27 14:41:12 +0700

    Add NSG security rules and IAM configuration

---
+++ infra/terraform/security.tf
@@ -0,0 +1,33 @@
+resource "azurerm_network_security_group" "nsg" {
+  name                = "inventory-nsg"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    Project = "InventoryCloudProject"
+  }
+}
