de19efab302a4fdb156e2829f09febfa482b6e2d
glend15 <stevans.glend15@gmail.com>
2026-04-27 12:54:43 +0700

    Menambahkan konfigurasi terraform untuk infrastruktur minggu-2

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "Malaysia West"
+}
+
+variable "admin_username" {
+  description = "VM username"
+  type        = string
+  default     = "azureuser"
+}
