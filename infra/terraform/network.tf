a2cdb575a0b903c74bb3b08e29eead5b2afa5500
glend15 <stevans.glend15@gmail.com>
2026-04-27 13:49:53 +0700

    Memperbaiki referensi grup network dan outputs

--- infra/terraform/network.tf
+++ infra/terraform/network.tf
@@ -1,8 +1,8 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = "inventory-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   tags = {
     Project = "InventoryCloudProject"
@@ -11,14 +11,14 @@ resource "azurerm_virtual_network" "vnet" {
 
 resource "azurerm_subnet" "public_subnet" {
   name                 = "public-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_subnet" "private_subnet" {
   name                 = "private-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
