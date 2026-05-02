1d6eaa041c065af6667037d92e941dab7cb12ae7
jertimothy233 <jertimothy233@gmail.com>
2026-05-02 17:38:46 +0700

    feat: update database config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql12');
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: "",
-    database: 'inventory_db'
+    host: 'mysql-inventory-kelompok2.mysql.database.azure.com',
+    user: 'dbadmin',
+    password: 'YOUR_PASSWORD',
+    database: 'smart_inventory'
 });
 
 module.exports = connection;
